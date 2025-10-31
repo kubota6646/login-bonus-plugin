260d70f83faa4eb8a798e51a070878115f1e7eb6
kubota6646 <240894337+kubota6646@users.noreply.github.com>
2025-10-31 18:08:06 +0900

    Add files via upload

---
+++ src/main/java/me/kubota6646/loginbonus/listeners/PlayerQuitListener.java
@@ -0,0 +1,22 @@
+package me.kubota6646.loginbonus.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.entity.Player;
+import me.kubota6646.loginbonus.managers.PlaytimeManager;
+
+public class PlayerQuitListener implements Listener {
+    private final PlaytimeManager playtimeManager;
+
+    public PlayerQuitListener(PlaytimeManager playtimeManager) {
+        this.playtimeManager = playtimeManager;
+    }
+
+    @Override
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        UUID uuid = player.getUniqueId();
+        // ログアウト時の処理（必要に応じて追加）
+    }
+}
